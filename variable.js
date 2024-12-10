817e1aca2abd6abba48576c9c6fb8776d2f2a5fd
mritunjay sukla <mritunjaysukla07@gmail.com>
2024-12-10 13:00:21 +0545

    variable explainantion with code in js

--- variable.js
+++ variable.js
@@ -1,1 +1,33 @@
-console.log("hello world");
+let name = "mritunjay"; // let is used for variables that might change...> block scoped 
+var age = 21; // var is old function scoped
+const country = "Nepal"; // used for variables which values wont change...> block scoped
+console.log(name);
+console.log(age);
+console.log(country);
+
+
+
+
+
+
+
+// block scope
+function textblockscope() {
+    if (true) {
+        let example = "hello world";
+        console.log(example); // this will be printed
+    }
+    console.log(example);// referenced error as message is not defined
+}
+textblockscope();
+
+
+
+// function scope
+function textblockscope() {
+    if (true) {
+        var example = "hello world";
+    }
+    console.log(example);// this will also be printed
+}
+textblockscope();
